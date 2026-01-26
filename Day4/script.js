d818d1eb16925482c30811dd0c5445884844b2f8
abhinavsarda11-create <abhinavsarda11@gmail.com>
2026-01-26 21:24:30 +0530

    Day4

---
+++ Day4/script.js
@@ -0,0 +1,13 @@
+let user={
+    FullName: "Abhinav",
+    address:"Sohna",
+    mobileno:8222951001,//no. is the least important so it came at last
+    favcolor:["black","white","blue"]
+};
+console.log(user["FullName"])
+//we can access the individual value by targeting key with variable name
+console.log(user.FullName,user.mobileno,user.favcolor[1]);//Abhinav 8222951001 white
+
+let date = new Date()
+console.log(date);
+
