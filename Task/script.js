530329f18648d9f218b15a4b6937d878f9dca70b
abhinavsarda11-create <abhinavsarda11@gmail.com>
2026-01-29 11:53:30 +0530

    Tasks

---
+++ Task/script.js
@@ -0,0 +1,14 @@
+//Task1
+const profile = document.getElementById("profile");
+
+profile.style.backgroundColor="tomato";
+profile.style.padding="15px";
+profile.style.textAlign="center";
+
+//Task2
+const highlight = document.getElementsByClassName("important");
+console.log(highlight);
+for (let i = 0; i < highlight.length; i++) {
+    highlight[i].style.color="red";
+    highlight[i].style.fontSize="20px";
+}
